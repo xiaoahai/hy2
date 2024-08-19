693cd2ba222634c3ba2528ea030e06320523face
xiaoahai <131226710+xiaoahai@users.noreply.github.com>
2024-08-19 21:42:32 +0800

    hy2.sh

--- hy2.sh
+++ hy2.sh
@@ -9,6 +9,6 @@ echo "1
 
 
 " > input
-wget -N --no-check-certificate https://github.com/seagullz4/hysteria2/raw/main/install.sh -o install.sh && chmod +x install.sh && bash install.sh < input
+wget -N --no-check-certificate https://github.com/seagullz4/hysteria2/raw/main/install.sh -o install.sh &&  bash install.sh < input
 
-chmod +x hysteria.sh
+chmod +x install.sh
